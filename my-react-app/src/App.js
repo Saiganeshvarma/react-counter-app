bd0df42f98f4b04116b1c3c45817351eec322235
saiganeshpalla <ganesh@stockholmitacademy.org>
2024-05-23 13:59:19 +0530

    frist commit

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -6,20 +6,28 @@ function App(){
   var [counter,setCount] = useState(0)
 
   return (
+    <div className='body'>
     <div className='wrapper'>
       <button className='minus' onClick={()=>{
         setCount(counter--)
       }}>-</button>
-      <pr>{counter}</pr>
+      <pr className="para">{counter}</pr>
       <button className='plus' onClick={()=>{
         setCount(counter++)
       }}>+</button>
 
-      <button onClick={()=>{
+      <button className='clear' onClick={()=>{
         setCount(0)
-
       }}>Clear</button>
+      <button onClick={()=>{
+        setCount(10)
+      }}>setCount</button>
+      <button onClick={()=>{
+        alert(`your count is ${counter}`)
+      }} >message</button>
     </div>
+    </div>
+  
   )
 }
 export default App
