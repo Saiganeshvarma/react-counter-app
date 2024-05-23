e127f85325bce793de69ab62df12072f09e3bda5
saiganeshpalla <ganesh@stockholmitacademy.org>
2024-05-23 13:36:58 +0530

    frist commit

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -3,20 +3,22 @@ import './App.css';
 import { useState } from 'react';
 
 function App(){
-  let [counter,setCounter] = useState(0)
+  var [counter,setCount] = useState(0)
 
-
-  return(
-    <div className="wrapper">
-      <button className="minus" onClick={()=>{
-        setCounter(counter--)
-
+  return (
+    <div className='wrapper'>
+      <button className='minus' onClick={()=>{
+        setCount(counter--)
       }}>-</button>
       <pr>{counter}</pr>
-      <button className="plus"onClick={()=>{
-        setCounter(counter++)
+      <button className='plus' onClick={()=>{
+        setCount(counter++)
+      }}>+</button>
 
-      }} >+</button>
+      <button onClick={()=>{
+        setCount(0)
+
+      }}>Clear</button>
     </div>
   )
 }
